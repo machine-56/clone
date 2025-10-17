14055c9b3721b2efd7ed3da8a03061208c4dda5b
machine-56 <itwasanoption@gmail.com>
2025-10-17 14:50:14 +0530

    debugging 4

--- connectly/settings.py
+++ connectly/settings.py
@@ -138,3 +138,6 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://clone-o47s.onrender.com']
+
